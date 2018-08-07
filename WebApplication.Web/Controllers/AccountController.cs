58f960abf51d3d1d2f333b1273efd05e708c193e
Josh Tucholski <josh@techelevator.com>
2018-08-07 13:40:51 -0400

    Initial commit.

---
+++ WebApplication.Web/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Web.Models.Account;
+using WebApplication.Web.Providers.Auth;
+
+namespace WebApplication.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthProvider authProvider;
+        public AccountController(IAuthProvider authProvider)
+        {
+            this.authProvider = authProvider;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            // Ensure the fields were filled out
+            if (ModelState.IsValid)
+            {
+                // Check that they provided correct credentials
+                bool validLogin = authProvider.SignIn(loginViewModel.Email, loginViewModel.Password);
+                if (validLogin)
+                {
+                    // Redirect the user where you want them to go after successful login
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult LogOff()
+        {
+            // Clear user from session
+            authProvider.LogOff();
+
+            // Redirect the user where you want them to go after logoff
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Register them as a new user (and set default role)
+                authProvider.Register(registerViewModel.Email, registerViewModel.Password, "user");
+
+                // Redirect the user where you want them to go after registering
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(registerViewModel);
+        }
+    }
+}
---
+++ WebApplication.Web/Providers/Auth/IAuthProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.Providers.Auth
+{
+    public interface IAuthProvider
+    {
+        /// <summary>
+        /// Returns the current signed in user.
+        /// </summary>
+        /// <returns></returns>
+        User GetCurrentUser();
+
+        /// <summary>
+        /// Signs a user in.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>True if the user signed in.</returns>
+        bool SignIn(string username, string password);
+
+        /// <summary>
+        /// Logs the user off from the system.
+        /// </summary>
+        void LogOff();
+
+        /// <summary>
+        /// Changes the logged in user's existing password.
+        /// </summary>
+        /// <param name="existingPassword"></param>
+        /// <param name="newPassword"></param>
+        /// <returns></returns>
+        bool ChangePassword(string existingPassword, string newPassword);
+
+        /// <summary>
+        /// Creates a new user in the system.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        void Register(string username, string password, string role);
+    }
+}
