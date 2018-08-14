f64cdf64e321a2537ceaf1e9b49e9a62654edb13
William Lawrence <Williamjlawrence1@gmail.com>
2018-08-14 14:43:01 -0400

    added dependency injection to location controller

--- WebApplication.Web/Controllers/LocationController.cs
+++ WebApplication.Web/Controllers/LocationController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Web.DAL;
 using WebApplication.Web.Models;
+using WebApplication.Web.Providers.Auth;
 
 namespace WebApplication.Web.Controllers
 {
     public class LocationController : Controller
     {
+        private readonly IAuthProvider authProvider;
+        private readonly IUserDAL dal;
 
-
-
+        public LocationController(IAuthProvider authProvider, IUserDAL dal)
+        {
+            this.authProvider = authProvider;
+            this.dal = dal;
+        }
 
         public JsonResult NearbyLocations(decimal latitude, decimal longitude)
         {
