90f18bee102e9c8acb8ae5e4a06752ba3a9f381d
William Lawrence <Williamjlawrence1@gmail.com>
2018-08-14 14:20:07 -0400

    Add location DAL and Interface. Added injection for LocationDAL to the database.

---
+++ WebApplication.Web/DAL/LocationSqlDAL.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.DAL
+{
+    public class LocationSqlDAL : ILocationDAL
+    {
+        /// <summary>
+        /// The connection string for the database.
+        /// </summary>
+        private readonly string connectionString;
+
+        public LocationSqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Gets all the locations that are within a set distance of the user.
+        /// </summary>
+        /// <param name="userLocation">The location of the user. Requires latitude and longitude.</param>
+        /// <param name="maxDistance">The max dstnace form the user in miles. </param>
+        /// <returns></returns>
+        public IList<Location> GetNeabyLocations(Location userLocation, double maxDistance)
+        {
+            IList<Location> nearbyLocations = new List<Location>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // The SQL query that is used to get all the locations that are near a certain locations
+                    string sql = "SELECT latitude, longitude, POW(69.1 * (latitude - @userLatitude), 2) + POW(69.1 * (@userLongitude - longitude) * COS(latitude / 57.3), 2) AS distance FROM TableName HAVING distance < @maxDistance ORDER BY distance; ";
+
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@userLatitude", userLocation.Latitude);
+                    command.Parameters.AddWithValue("@userLongitude", userLocation.Longitude);
+                    command.Parameters.AddWithValue("@maxDistance", maxDistance);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while(reader.Read())
+                    {
+                        nearbyLocations.Add(MapRowtoLocation(reader));
+                    }
+                    
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+
+            return nearbyLocations;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reader">The reader that is being used to map the info in the database to the location object.</param>
+        /// <returns></returns>
+        private Location MapRowtoLocation(SqlDataReader reader)
+        {
+            Location location = new Location
+            {
+                Id = Convert.ToInt32(reader["id"]),
+                Latitude = Convert.ToDecimal(reader["latitude"]),
+                Longitude = Convert.ToDecimal(reader["longitude"])
+            };
+
+            return location;
+        }
+    }
+}
