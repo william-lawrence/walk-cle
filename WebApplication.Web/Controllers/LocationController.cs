d9113d4857decef12f633a7e1d59b9623cfa290a
William Lawrence <Williamjlawrence1@gmail.com>
2018-08-14 14:39:24 -0400

    Added Detail action and view

--- WebApplication.Web/Controllers/LocationController.cs
+++ WebApplication.Web/Controllers/LocationController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Web.Models;
 
 namespace WebApplication.Web.Controllers
 {
     public class LocationController : Controller
     {
-        public IActionResult (decimal latitude, decimal longitude)
+
+
+
+
+        public JsonResult NearbyLocations(decimal latitude, decimal longitude)
         {
+            List<Location> locations = new List<Location>();
+
+
+
             return Json(locations);
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
         }
     }
 }
