883fe7fb447c5b83da8c28ff49c27d11ddba1dc3
Sarah Robertson <robertson_sarah@yahoo.com>
2018-08-16 15:05:27 -0400

    Added nav bar, location count radio buttons, styled detail page, added Search Controller and Category View

---
+++ WebApplication.Web/Controllers/SearchController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Web.DAL;
+using WebApplication.Web.Models;
+using WebApplication.Web.Providers.Auth;
+
+namespace WebApplication.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+		private readonly ICategorySqlDAL dal;
+		private readonly ILocationDAL locationDal;
+
+		public SearchController(ICategorySqlDAL dal, ILocationDAL locationDal)
+		{
+			this.dal = dal;
+			this.locationDal = locationDal;
+		}
+
+        public IActionResult Category(string category)
+        {
+			IList<Location> locations = new List<Location>();
+
+			locations = dal.CategorySearch(category);
+
+            return View(locations);
+        }
+    }
+}
