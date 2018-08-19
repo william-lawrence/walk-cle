a40f43dc3625efe7f86ad7991fb8c521608d7e06
William Lawrence <williamjlawrence1@gmail.com>
2018-08-19 11:41:29 -0400

    Added documentation and did some light refactor to the SearchController

--- WebApplication.Web/Controllers/SearchController.cs
+++ WebApplication.Web/Controllers/SearchController.cs
@@ -11,31 +11,55 @@ namespace WebApplication.Web.Controllers
 {
     public class SearchController : Controller
     {
-		private readonly ICategorySqlDAL dal;
+        /// <summary>
+        /// The DAL used to get category information from the database. 
+        /// </summary>
+		private readonly ICategorySqlDAL categroyDal;
+
+        /// <summary>
+        /// The DAL used to get location information from the database. 
+        /// </summary>
 		private readonly ILocationDAL locationDal;
 
-		public SearchController(ICategorySqlDAL dal, ILocationDAL locationDal)
+        /// <summary>
+        /// Constructor used to inject category DAL and location DAL
+        /// </summary>
+        /// <param name="categoryDal">The category DAL to use.</param>
+        /// <param name="locationDal">The location DAL to use.</param>
+		public SearchController(ICategorySqlDAL categoryDal, ILocationDAL locationDal)
 		{
-			this.dal = dal;
+			this.categroyDal = categoryDal;
 			this.locationDal = locationDal;
 		}
 
+        /// <summary>
+        /// Searches for locations by category
+        /// </summary>
+        /// <param name="cat">The category to search by</param>
+        /// <returns>A list of all the loccations that fall under a particular category as JSON</returns>
 		[HttpGet]
         public JsonResult CategorySearch(string cat)
         {
 			IList<Location> locations = new List<Location>();
 
-			locations = dal.CategorySearch(cat);
+			locations = categroyDal.CategorySearch(cat);
 
             return Json(locations);
         }
 
+        /// <summary>
+        /// Returns the view for a the results of a category search.
+        /// </summary>
+        /// <param name="cat">The category to to search by</param>
+        /// <returns>A view with lacations that fall under the searched category</returns>
 		public IActionResult Category(string cat)
-		{
-			Category category = new Category();
-			category.Name = cat;
+        {
+            Category category = new Category
+            {
+                Name = cat
+            };
 
-			return View(category);
-		}
+            return View(category);
+        }
 	}
 }
