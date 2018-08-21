6c8b2b626e0fbe7f2de64cae321da965b340de1c
William Lawrence <Williamjlawrence1@gmail.com>
2018-08-21 10:53:04 -0400

    add scaffold to profile.js

--- WebApplication.Web/wwwroot/js/profile.js
+++ WebApplication.Web/wwwroot/js/profile.js
@@ -1,1 +1,11 @@
-﻿
+﻿function getElementFromTemplate(id) {
+    let domNode = document.importNode(document.getElementById(id).content, true).firstElementChild;
+
+    return domNode;
+}
+
+// Ensures that the page is fully loaded before scripts run.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM Loaded');
+    
+});
